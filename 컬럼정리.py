9d084e116644e0030aed47dc60511b91d87778df
kang-narae <raenakang111@gmail.com>
2022-04-04 14:05:51 +0900

    데이터 concat 완료

--- 컬럼정리.py
+++ 컬럼정리.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import glob
-data_paths = glob.glob('./datasets/*')
+data_paths = glob.glob('./datasets/nonwell/*')
 df = pd.DataFrame()
 for paths in data_paths:
     paths = glob.glob(paths)
@@ -9,15 +9,17 @@ for paths in data_paths:
         print(path)
         for i in path:
             print(i)
-            # df_temp = pd.read_csv(i)
+            df_temp = pd.read_csv(i)
             # df_temp = df_temp.reset_index()
-            # print(df.head())
-            # print(df.info())
-            # df_temp.columns = ['title', 'content', 'department']
-            # df = pd.concat([df, df_temp], axis='rows')
+            df_temp.columns = ['index','title','content','department']
+            df_temp= df_temp.drop(['index'], axis = 1)
+            print(df_temp.head())
+            print(df_temp.info())
+            df_temp.columns = ['title', 'content', 'department']
+            df = pd.concat([df, df_temp], axis='rows')
 
 
 # print(df.head())
 # df.info()
 # #
-# df.to_csv('./medical_qs_Classification5.csv', index = False)
+df.to_csv('./datasets/concat_all_nunwell.csv', index = False)
